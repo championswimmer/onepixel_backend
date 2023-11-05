10569044cd9bc9c4f0c197bbe15e34e821a9fbdc
Lohit Marodia <lohitmarodia@hotmail.com>
2023-11-05 17:20:04 +0530

    return user when created

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -20,30 +20,17 @@ func NewUsersController(db *gorm.DB) *UsersController {
 	}
 }
 
-// FindUserByEmail checks if a user exists with the given email
-func (c *UsersController) FindUserByEmail(email string) (*models.User, error) {
-	user := &models.User{}
-	result := c.db.Where("email = ?", email).First(user)
-	if result.Error != nil && !errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		// Error occurred during the lookup, and it's not because the record wasn't found
-		return nil, result.Error
-	}
-	// No error or record not found is expected as we want to check existence
-	return user, result.Error
-}
-
-
 // Create new user
-func (c *UsersController) Create(email string, password string) error {
+func (c *UsersController) Create(email string, password string) (*models.User, error) {
 	// Check if email is already registered
 	existingUser, err := c.FindUserByEmail(email)
 	if err == nil && existingUser.ID != 0 {
 		// User exists and ID is populated, hence email is already registered
-		return errors.New("email already registered")
+		return nil, errors.New("email already registered")
 	}
 	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
 		// There was an actual error in looking up the user
-		return err
+		return nil, err
 	}
 
 	// Hash the password
@@ -57,10 +44,10 @@ func (c *UsersController) Create(email string, password string) error {
 	// Save the user to the database
 	res := c.db.Create(user)
 	if res.Error != nil {
-		return res.Error
+		return nil, res.Error
 	}
 
-	return nil
+	return user, nil
 }
 
 // FindUserByEmail find user by email
