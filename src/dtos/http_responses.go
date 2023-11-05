ce5b91a94118c4dd3321495e65d6b8944534fc7e
FaizBShah <faizbshah2001@gmail.com>
2023-11-05 21:55:37 +0530

    chore: added error response dto and used it in the registerUser route, and also added tests for it

--- src/dtos/http_responses.go
+++ src/dtos/http_responses.go
@@ -7,9 +7,21 @@ type UserResponse struct {
 	Email string `json:"email"`
 }
 
+type ErrorResponse struct {
+	Status  uint   `json:"status"`
+	Message string `json:"message"`
+}
+
 func UserResponseFromUser(user *models.User) UserResponse {
 	return UserResponse{
 		ID:    user.ID,
 		Email: user.Email,
+	}
+}
+
+func GetErrorResponse(status uint, message string) ErrorResponse {
+	return ErrorResponse{
+		Status:  status,
+		Message: message,
 	}
 }
