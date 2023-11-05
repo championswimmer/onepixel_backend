54ab4b8532b612eb20962b7f0cf7014d3d5c55df
shivamc489 <shivamc199941@gmail.com>
2023-11-05 23:33:40 +0530

    fixed locals and params issue

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -1,54 +1,53 @@
 package controllers
 
 import (
-	"onepixel_backend/src/models"
+  "onepixel_backend/src/models"
 
-	"gorm.io/gorm"
+  "gorm.io/gorm"
 )
 
 type UsersController struct {
-	// db
-	db *gorm.DB
+  // db
+  db *gorm.DB
 }
 
 func NewUsersController(db *gorm.DB) *UsersController {
-	return &UsersController{
-		db: db,
-	}
+  return &UsersController{
+    db: db,
+  }
 }
 
 // Create new user
 func (c *UsersController) Create(email string, password string) (*models.User, error) {
-	user := &models.User{
-		Email:    email,
-		Password: password, // TODO: hash password
-	}
-	res := c.db.Create(user)
-	if res.Error != nil {
-		return nil, res.Error
-	}
-	return user, nil
+  user := &models.User{
+    Email:    email,
+    Password: password, // TODO: hash password
+  }
+  res := c.db.Create(user)
+  if res.Error != nil {
+    return nil, res.Error
+  }
+  return user, nil
 }
 
 // Update user details
-func (c *UsersController) Update(password string) (*models.User, error) {
-	user := c.Locals("users")
-	user.password = password
-	res := c.db.Save(&user)
-	if res.Error != nil {
-		return nil, res.Error
-	}
-	return user, nil
+func (c *UsersController) Update(user *models.User, password string) (*models.User, error) {
+  user.Password = password
+  res := c.db.Save(&user)
+  if res.Error != nil {
+    return nil, res.Error
+  }
+  return user, nil
 }
 
 // FindUserByEmail find user by email
 func (c *UsersController) FindUserByEmail(email string) (*models.User, error) {
-	user := &models.User{
-		Email: email,
-	}
-	res := c.db.Where(user).First(user)
-	if res.Error != nil {
-		return nil, res.Error
-	}
-	return user, nil
+  user := &models.User{
+    Email: email,
+  }
+  res := c.db.Where(user).First(user)
+  if res.Error != nil {
+    return nil, res.Error
+  }
+  return user, nil
 }
--- src/routes/api/users.go
+++ src/routes/api/users.go
@@ -1,71 +1,80 @@
 package api
 
 import (
-	"errors"
-	"gorm.io/gorm"
-	"onepixel_backend/src/auth"
-	"onepixel_backend/src/controllers"
-	"onepixel_backend/src/dtos"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/samber/lo"
+  "errors"
+  "gorm.io/gorm"
+  "onepixel_backend/src/auth"
+  "onepixel_backend/src/controllers"
+  "onepixel_backend/src/dtos"
+  "onepixel_backend/src/models"
+  "github.com/gofiber/fiber/v2"
+  "github.com/samber/lo"
+  "strconv"
 )
 
 var usersController *controllers.UsersController
 
 // UsersRoute /api/v1/users
 func UsersRoute(db *gorm.DB) func(router fiber.Router) {
-	usersController = controllers.NewUsersController(db)
-	return func(router fiber.Router) {
-		router.Post("/", registerUser)
-		router.Post("/login", loginUser)
-		router.Get("/:id", auth.MandatoryAuthMiddleware, getUserInfo)
-		router.Patch("/:id", auth.MandatoryAuthMiddleware, updateUserInfo)
-	}
+  usersController = controllers.NewUsersController(db)
+  return func(router fiber.Router) {
+    router.Post("/", registerUser)
+    router.Post("/login", loginUser)
+    router.Get("/:id", auth.MandatoryAuthMiddleware, getUserInfo)
+    router.Patch("/:id", auth.MandatoryAuthMiddleware, updateUserInfo)
+  }
 }
 
 func registerUser(ctx *fiber.Ctx) error {
-	var u = new(dtos.CreateUserRequest)
-	// TODO: handle error and show Bad-Request to client
-	lo.Must0(ctx.BodyParser(u))
+  var u = new(dtos.CreateUserRequest)
+  // TODO: handle error and show Bad-Request to client
+  lo.Must0(ctx.BodyParser(u))
 
-	savedUser, err := usersController.Create(u.Email, u.Password)
-	if err != nil {
-		if errors.Is(err, gorm.ErrDuplicatedKey) {
-			// TODO: make a Error response DTO
-			return ctx.Status(fiber.StatusConflict).JSON(fiber.Map{
-				"message": "User with this email already exists",
-			})
-		}
-	}
+  savedUser, err := usersController.Create(u.Email, u.Password)
+  if err != nil {
+    if errors.Is(err, gorm.ErrDuplicatedKey) {
+      // TODO: make a Error response DTO
+      return ctx.Status(fiber.StatusConflict).JSON(fiber.Map{
+        "message": "User with this email already exists",
+      })
+    }
+  }
 
-	return ctx.Status(fiber.StatusCreated).JSON(dtos.UserResponseFromUser(savedUser))
+  return ctx.Status(fiber.StatusCreated).JSON(dtos.UserResponseFromUser(savedUser))
 }
 
 func loginUser(ctx *fiber.Ctx) error {
-	return ctx.SendString("LoginUser")
+  return ctx.SendString("LoginUser")
 }
 
 func getUserInfo(ctx *fiber.Ctx) error {
-	return ctx.SendString("GetUserInfo")
+  return ctx.SendString("GetUserInfo")
 }
 
 func updateUserInfo(ctx *fiber.Ctx) error {
-	var u = new(dtos.UpdateUserRequest)
-	lo.Must0(ctx.BodyParser(u))
+  var u = new(dtos.UpdateUserRequest)
+  lo.Must0(ctx.BodyParser(u))
+  idStr := ctx.Params("id")
+  id, err := strconv.ParseUint(idStr, 10, 64)
 
-	if c.Locals("user").id != c.Params["id"] {
-		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"message": "You are not allowed to perform this action",
-		})
-	}
+  user, ok := ctx.Locals("user").(*models.User)
+  if !ok {
+    return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+        "message": "Internal Server Error",
+    })
+  }
+  if user.ID != uint(id) {
+    return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+      "message": "You are not allowed to perform this action",
+    })
+  }
 
-	savedUser, err := usersController.Update(u.Password)
-	if err != nil {
-		return ctx.Status(fiber.StatusConflict).JSON(fiber.Map{
-			"message": err,
-		})
-	}
+  updatedUser, err := usersController.Update(user, u.Password)
+  if err != nil {
+    return ctx.Status(fiber.StatusConflict).JSON(fiber.Map{
+      "message": err,
+    })
+  }
 
-	return ctx.Status(fiber.StatusOK).JSON(dtos.UserResponseFromUser(savedUser))
+  return ctx.Status(fiber.StatusOK).JSON(dtos.UserResponseFromUser(updatedUser))
 }
