40afe00dc49c87c42396d581ea64d274255739c8
Arnav Gupta <championswimmer@gmail.com>
2024-02-23 23:12:25 +0530

    infra: attempt to connect with postgres/clickhouse 10 times
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- src/db/providers.go
+++ src/db/providers.go
@@ -6,14 +6,40 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"onepixel_backend/src/utils/applogger"
+	"time"
 )
+
+var (
+	_numAttempts = 10
+	_delay       = 1 * time.Second
+	_openSync    = lo.Synchronize()
+)
+
+func attemptToOpen[T any](opener func() (*T, error)) *T {
+	var t *T
+	_, _, e := lo.AttemptWithDelay(_numAttempts, _delay, func(i int, d time.Duration) (err error) {
+		applogger.Info("Opening attempt ", i)
+		_openSync.Do(func() {
+			t, err = opener()
+		})
+		return
+	})
+	if t == nil {
+		applogger.Panic("Failed to open ", e.Error())
+	}
+	return t
+}
 
 func ProvidePostgresDB(dbUrl string, config *gorm.Config) *gorm.DB {
 	applogger.Warn("App: Using postgres db")
-	return lo.Must(gorm.Open(postgres.Open(dbUrl), config))
+	return attemptToOpen(func() (*gorm.DB, error) {
+		return gorm.Open(postgres.Open(dbUrl), config)
+	})
 }
 
 func ProvideClickhouseDB(dbUrl string, config *gorm.Config) *gorm.DB {
 	applogger.Warn("App: Using clickhouse db")
-	return lo.Must(gorm.Open(clickhouse.Open(dbUrl), config))
+	return attemptToOpen(func() (*gorm.DB, error) {
+		return gorm.Open(clickhouse.Open(dbUrl), config)
+	})
 }
